495f1b6acdbd723ba2197b2736d95d6484051f98
sararetta <74893402+sararetta@users.noreply.github.com>
2022-10-16 19:45:06 +0300

    Update productExceptSelf.js

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -4,35 +4,18 @@
  */
  var productExceptSelf = function(nums) {
     let arr=[]
+    let prevElement=1;
     for (let i=0; i< nums.length; i++) {
         let product=1;
-        if(i==nums.length-2){
-            product*=nums[i+1];
-            let arr1=arr.slice(0,i);
-            for (let k=0; k< arr1.length; k++) {
-                product*=nums[k];
-            }
-            product==-0?arr.push(0):arr.push(product);
-
-        }
-        else if(i==nums.length-1){
-            let arr1=arr.slice(0,i);
-            for (let k=0; k< arr1.length; k++) {
-                product*=nums[k];
-            }
-            product==-0?arr.push(0):arr.push(product);
-        }
-        else{
             for (let j=i+1; j< nums.length; j++) {
                 product*=nums[j];
             }
-            let arr1=arr.slice(0,i);
-            for (let k=0; k< arr1.length; k++) {
-                product*=nums[k];
+            if(i!=0){
+                let index=nums[i-1];
+                prevElement=prevElement*index;
             }
-            product==-0?arr.push(0):arr.push(product);
-        }
+            prevElement==-0?prevElement=0:prevElement=prevElement;
+            product==-0?arr.push(0):arr.push(product*prevElement);
     }
     return arr;
-    
 };
